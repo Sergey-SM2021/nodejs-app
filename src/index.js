f7f4fbac2bcae4c1e3ae548577a6521a2ed6a12b
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-07-13 17:18:39 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 const port = 7000;
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get("/", (req, res) => res.send("hello world!"));
 
 app.listen(port)
